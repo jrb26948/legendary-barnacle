9c45ceb8ed748e7ce5d57768fcfb0d7215f895d9
jrb26948 <48367485+jrb26948@users.noreply.github.com>
2019-04-05 10:43:33 -0400

    Add files via upload

---
+++ receiver.py
@@ -0,0 +1,42 @@
+#Receives JPEG compressed live video over UDP
+import socket
+import numpy
+import time
+import cv2
+
+#Set IP and Port
+IP = ""
+PORT = 5005
+
+#Initialize frame count
+framecounter = 0
+
+#Specify UDP and bind.
+sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+sock.bind ((IP, PORT))
+
+#Loop receiving frames
+while True:
+
+    #Get length from first packet
+    length, addr = sock.recvfrom(5)
+    length = int(length.decode())
+    
+    #Use length to determine frame size
+    data, addr = sock.recvfrom(length)
+    
+    #Turn JPEG string back into image
+    frame = numpy.fromstring(data,dtype=numpy.dtype('uint8'))
+    frame = cv2.imdecode(frame, -1)
+    frame = frame.reshape(240,320,3)
+    
+    #Display frame
+    cv2.imshow('Receiving', frame)
+    framecounter+=1
+    print("Frames received: ", framecounter)
+    #End when 'q' is pressed
+    if cv2.waitKey(1) & 0xFF == ord ('q'):
+        break
+
+#Close display
+cv2.destroyAllWindows()
---
+++ transmitter.py
@@ -0,0 +1,50 @@
+#Transmits live video over UDP with JPEG compression
+import numpy as np
+import cv2
+import socket
+
+#Set IP and Port
+IP = "192.168.0.3"
+PORT = 5005
+
+#Initialize webcam with capture resolution
+cap = cv2.VideoCapture(0)
+cap.set(3,320)
+cap.set(4,240)
+
+#Initialize frame count
+framecounter = 0
+
+#Loop the Transmission 
+while(True):
+
+    #Return good capture frames and display them
+    ret, frame = cap.read()
+    cv2.imshow('Transmitting', frame)
+
+    #Encode each frame as a JPEG
+    encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
+    result, encimg = cv2.imencode('.jpg', frame, encode_param)
+
+    #Create length packet
+    l = len(encimg)
+    l = str.encode(str(l).zfill(5))
+
+    #Specify UDP as protocol
+    sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+
+    #Send length packet
+    sock.sendto((l),(IP,PORT))
+
+    #Send JPEG and count frames sent
+    sock.sendto((encimg),(IP, PORT))
+    framecounter+=1
+    print("Frames sent: ", framecounter)
+
+    #End when 'q' is pressed
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+#Release capture and close display
+cap.release()
+cv2.destroyAllWindows()
